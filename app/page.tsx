45986045c8577c3f758a951b8fb17c52327263ea
omegamaster86 <masya868686@gmail.com>
2024-08-17 19:19:44 +0900

    Linkの設定

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Link href={"/useActionStateUseTransition"}>移動</Link>
+      <Link href={"/useActionStateUseTransition"}>useActionStateUseTransition</Link>
     </div>
   );
 }
