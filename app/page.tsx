bde4fdebdf0bc8956019436b79e784e20225c161
omegamaster86 <masya868686@gmail.com>
2024-09-24 09:44:32 +0900

    useContextへのリンクを追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div className='mt-4 ml-10'>
-        <Link href={"/useActionStateUseTransition"}>useActionStateUseTransitionへ移動</Link>
+      <div >
+        <div className='mt-4 ml-10'>
+          <Link href={"/useActionStateUseTransition"}>useActionStateUseTransitionへ移動</Link>
+          <Link href={"/useContext"}>useContextへ移動</Link>
+        </div>
       </div>
     </div>
   );
