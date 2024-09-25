fc97384dda814330a23235c77a19c62f138d28d3
omegamaster86 <masya868686@gmail.com>
2024-09-25 22:53:24 +0900

    cardを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import Link from "next/link";
+import { Card, CardHeader, Heading } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <div>
       <div >
         <div className='mt-4 ml-10'>
-          <Link href={"/useActionStateUseTransition"}>useActionStateUseTransitionへ移動</Link>
-          <Link href={"/useContext"}>useContextへ移動</Link>
+        <Card maxW='sm'>
+          <CardHeader>
+            <Heading size='md'><Link href={"/useActionStateUseTransition"}>useActionState/UseTransitionへ移動</Link></Heading>
+          </CardHeader>
+        </Card>
+        <Card maxW='sm' className='mt-5'>
+          <CardHeader>
+            <Heading size='md'><Link href={"/useContext"}>useContextへ移動</Link></Heading>
+          </CardHeader>
+        </Card>
+          {/* <Link href={"/useActionStateUseTransition"}>useActionStateUseTransitionへ移動</Link>
+          <Link href={"/useContext"}>useContextへ移動</Link> */}
         </div>
       </div>
     </div>
