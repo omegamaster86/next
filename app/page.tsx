1342a7cb1ec859535bb9f4b041f0e0bf3a72770c
omegamaster86 <masya868686@gmail.com>
2024-09-28 19:02:33 +0900

    不要なdivを削除

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,17 @@ import { Card, CardHeader, Heading } from '@chakra-ui/react'
 export default function Home() {
   return (
     <div>
-      <div >
-        <div className='mt-4 ml-10'>
-          <Card maxW='sm'>
-            <CardHeader>
-              <Heading size='md'><Link href={"/useActionStateUseTransition"}>useActionState/UseTransitionへ移動</Link></Heading>
-            </CardHeader>
-          </Card>
-          <Card maxW='sm' className='mt-5'>
-            <CardHeader>
-              <Heading size='md'><Link href={"/useContext"}>useContextへ移動</Link></Heading>
-            </CardHeader>
-          </Card>
-        </div>
+      <div className='mt-4 ml-10'>
+        <Card maxW='sm'>
+          <CardHeader>
+            <Heading size='md'><Link href={"/useActionStateUseTransition"}>useActionState/UseTransitionへ移動</Link></Heading>
+          </CardHeader>
+        </Card>
+        <Card maxW='sm' className='mt-5'>
+          <CardHeader>
+            <Heading size='md'><Link href={"/useContext"}>useContextへ移動</Link></Heading>
+          </CardHeader>
+        </Card>
       </div>
     </div>
   );
