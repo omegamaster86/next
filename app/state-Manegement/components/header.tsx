49e933752c3f1e17397947c85f50818123daed56
omegamaster86 <masya868686@gmail.com>
2024-09-29 17:14:41 +0900

    component化

---
+++ app/state-Manegement/components/header.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React, {type FC} from 'react'
+
+export const Header: FC = () => {
+  return (
+    <div className='flex p-10 w-full'>
+      <h1 className='font-bold w-4/6'>
+        <Link href={"/state-Manegement"}>Reaxt状態管理</Link>
+      </h1>
+      <h1 className='font-bold space-x-10'>
+        <Link href={"/state-Manegement"}>TODO一覧</Link>
+        <Link href={"/state-Manegement/todo-add"}>TODO追加</Link>
+      </h1>
+    </div>
+  )
+}
