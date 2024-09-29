a39dca9ebf84ff518fb6cc93ed7542a4d4981b9b
omegamaster86 <masya868686@gmail.com>
2024-09-29 16:56:32 +0900

    Link先の設定

--- app/state-Manegement/page.tsx
+++ app/state-Manegement/page.tsx
@@ -5,11 +5,11 @@ const page = () => {
   return (
     <div className='flex p-10 w-full'>
       <h1 className='font-bold w-4/6'>
-        <Link href={"/"}>Reaxt状態管理</Link>
+        <Link href={"/state-Manegement"}>Reaxt状態管理</Link>
       </h1>
       <h1 className='font-bold space-x-10'>
-        <Link href={"/"}>TODO一覧</Link>
-        <Link href={"/add"}>TODO追加</Link>
+        <Link href={"/state-Manegement"}>TODO一覧</Link>
+        <Link href={"state-Manegement//todo-add"}>TODO追加</Link>
       </h1>
     </div>
   )
