d26b0835240d435cda2ec872312235bfdf3ab0fc
omegamaster86 <masya868686@gmail.com>
2024-09-29 17:57:53 +0900

    TODO一覧を追加

--- app/state-Manegement/page.tsx
+++ app/state-Manegement/page.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import { Header } from './components/header'
 
+const TODOS = [
+  { id: 1, text: "foo1", isDone: false },
+  { id: 2, text: "foo2", isDone: true },
+];
+
 const page = () => {
   return (
-    <div className='flex p-10 w-full'>
+    <div>
       <Header />
+      <h3 className='font-bold text-4xl text-center mt-6'>TODO一覧</h3>
+      {TODOS.map((todo) => (
+        <div key={todo.id} className='text-center'>
+          <label className='text-3xl'>
+            <input 
+            type="checkbox"
+            checked={todo.isDone}
+            className='w-4 h-4 mt-6'
+            />
+            {todo.text}
+          </label>
+        </div>
+      ))}
     </div>
   )
 }
