a7e1814bab1af825c222fcba4e0ca8ba9f3c63ea
omegamaster86 <masya868686@gmail.com>
2024-09-29 16:45:05 +0900

    header追加

--- app/state-Manegement/page.tsx
+++ app/state-Manegement/page.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <h1>
-            <Link href={"/"}>Reaxt状態管理</Link>
-          </h1>
-        </nav>
-      </header>
+    <div className='flex p-10 w-full'>
+      <h1 className='font-bold w-4/6'>
+        <Link href={"/"}>Reaxt状態管理</Link>
+      </h1>
+      <h1 className='font-bold space-x-10'>
+        <Link href={"/"}>TODO一覧</Link>
+        <Link href={"/add"}>TODO追加</Link>
+      </h1>
     </div>
   )
 }
