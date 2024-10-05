9a9561a260a657cbbd3d302aac5aad5423579759
omegamaster86 <masya868686@gmail.com>
2024-10-05 21:02:53 +0900

    todoの受け渡し

---
+++ app/useState/layout.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useState } from "react";
+import type { Todo } from "./types";
+import Page from "./page";
+
+const TODOS: Todo[] = [
+  { id: 1, text: "foo1", isDone: false },
+  { id: 2, text: "foo2", isDone: true },
+];
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [todos, setTodos] = useState<Todo[]>(TODOS);
+
+  return (
+    <html lang='en'>
+      <body>
+        <Page todos={todos} setTodos={setTodos}/>
+      </body>
+    </html>
+  );
+}
