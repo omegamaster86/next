f58096690f562505e1864d7fe99306eac3dc4246
omegamaster86 <masya868686@gmail.com>
2024-10-13 23:33:53 +0900

    不要なコメントアウトを削除

--- app/useState/components/header.tsx
+++ app/useState/components/header.tsx
@@ -13,10 +13,6 @@ export const Header: FC<Props> = ({ todoCount }) => {
         <h1 className='font-bold w-4/6'>
           <Link href={"/useState"}>Reaxt状態管理</Link>
         </h1>
-        {/* <h1 className='font-bold space-x-10'>
-          <Link href={"/useState"}>TODO一覧</Link>
-          <Link href={"/useState/todo-add"}>TODO追加</Link>
-        </h1> */}
       </div>
       <TodoCounter todoCount={todoCount} />
     </>
