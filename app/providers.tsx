8ee558d3b3682171c2d570dd29b7c138d8772d94
omegamaster86 <masya868686@gmail.com>
2024-12-15 19:00:55 +0900

    フォーマット変更

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+	return <ChakraProvider>{children}</ChakraProvider>;
 }
